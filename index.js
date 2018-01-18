d0f982671502a5a40c2775db4577a245dd3596e1
SDS 800 <jeremyhyman@gmail.com>
2018-01-18 16:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,10 +60,9 @@ function total() {
 }
 
 function removeFromCart(item) {
-  const index = cart.indexOf(item);
     for (var i = 0; i < 101; i++) {
        if ([i] === (Object.values(cart[i])[0])) {
-      cart.splice(indexOf(Object.values(cart[i])[0])), 1);
+      cart.splice(indexOf(Object.values(cart[i])[0]), 1);
          
         }
 else{
